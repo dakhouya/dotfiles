c6c2058a9b7f670bae315bc585d256b2f3203b7e
David Khouya <dakhouya@gmail.com>
2020-02-24 09:21:26 -0500

    Add ohmyzsh

--- install.sh
+++ install.sh
@@ -12,6 +12,21 @@ ln -sf $DOTFILES_DIR/.vim $HOME/.vim
 ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
 vim +PlugInstall +qall
 
+echo "Install zsh files"
+if ! which curl; then
+    echo "Install curl before running this script"
+    exit 1
+fi
+
+if ! which zsh; then
+    echo "Install zsh before running this script"
+    exit 1
+fi
+
+if ! sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+    echo "Fail to install ohmyzsh"
+fi
+
 echo "Install git files"
 ls -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 
