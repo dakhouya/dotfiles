796b6f6b829e29d95bdafec87fad277a244105aa
David Khouya <dakhouya@gmail.com>
2025-08-24 21:49:20 -0400

    feat: remove powershell script

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt paradox
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
